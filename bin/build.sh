135fcbaac03ac25b494386ae7039bb5b8818a826
James Cuzella <james.cuzella@lyraphase.com>
2017-02-20 02:29:01 -0700

    Add settings.py; Set docker build-arg for TOASTER_ADMIN_NAME, TOASTER_ADMIN_EMAIL

--- bin/build.sh
+++ bin/build.sh
@@ -5,10 +5,15 @@ REPO_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 
 [ -d "${REPO_BASE}/bitbake/.git" ] || git submodule update --init --recursive
 
+# Set ADMINS in settings.py
+if [ -n "$TOASTER_ADMIN_NAME" -a -n "$TOASTER_ADMIN_EMAIL" ]; then
+  BUILD_ARGS="--build-arg TOASTER_ADMIN_NAME=$TOASTER_ADMIN_NAME --build-arg TOASTER_ADMIN_EMAIL=$TOASTER_ADMIN_EMAIL"
+fi
+
 # Generate random secret key each time in build image
 cat ${REPO_BASE}/settings.py | sed -e "s#SECRET_KEY.*#SECRET_KEY = '$(openssl rand -base64 64 | tr -d '\n')'#" > ${REPO_BASE}/bitbake/lib/toaster/toastermain/settings.py
 
 cd ${REPO_BASE}/bitbake
-docker build -t trinitronx/yocto-bitbake-toaster:latest -f ../Dockerfile .
+docker build $BUILD_ARGS -t trinitronx/yocto-bitbake-toaster:latest -f ../Dockerfile .
 
 docker run -ti -v $(pwd)/data:/data -p 3800:80 trinitronx/yocto-bitbake-toaster:latest
